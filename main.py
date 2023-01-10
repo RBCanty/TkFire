5eee662fd2f8fd70c39813ab8aec0a699bb3b3bc
rcanty <rcanty@mit.edu>
2023-01-10 17:20:49 -0500

    Updated Dispatcher to be more flexible and added the ability to run POST commands

--- main.py
+++ main.py
@@ -3,44 +3,58 @@
 @author: Ben C
 """
 
-import tkfire
-from tkfire import tk, LAYOUT, TYPE, CHILDREN, LB33, TB33, BOTH33, PACK_SCROLL
+
+from tkfire import *
+import tkinter as tk
+
 
 from pprint import pprint
 import yaml
 from io import StringIO
 
 
-# Step 1, create a Tk or TopLevel object for the GUI
-core = tk.Tk()
+BOTH33 = {'fill': 'both', 'ipadx': 3, 'ipady': 3}
+TB33 = {'side': 'top', 'fill': 'both', 'ipadx': 3, 'ipady': 3}
+LB33 = {'side': 'left', 'fill': 'both', 'ipadx': 3, 'ipady': 3}
 
-# Step 2, Create variables or other functions which will plug into the GUI
+
+def print_hello():
+    print("Hello")
+
+
+# #### Step 1 ####
+# create a Tk or TopLevel object for the GUI
+tk_core = tk.Tk()
+
+
+# #### Step 2 ####
+# Create variables or other functions which will plug into the GUI
 memory = dict()
-memory['Opt1'] = tk.IntVar
-memory['Opt2'] = tk.IntVar
-memory['Opt3'] = tk.IntVar
+n_options = 3
+for i in range(0, n_options):
+    memory[f'Opt{i+1}'] = tk.IntVar
 memory['options'] = [1, 2, 3, 4, 5]
-memory['btn_cmd'] = lambda: print("Hello")
+memory['btn_cmd'] = lambda: print("Hello!")
 
 
-# Step 2b, create any custom widgets you may need
+# #### Step 2b ####
+# create any custom widgets you may need
 class YamlBox:
     def __init__(self, core):
         self.core = core
         self.frame = tk.Frame(self.core)
-        # self.frame.pack(**TB33)
         self.container = tk.Frame(self.frame)
         self.container.pack(**TB33)
         self.scrolly = tk.Scrollbar(self.container)
         self.scrollx = tk.Scrollbar(self.container, orient='horizontal')
-        self.scrolly.pack(PACK_SCROLL)
+        self.scrolly.pack(side=tk.RIGHT, fill=tk.Y)
         self.scrollx.pack(side=tk.BOTTOM, fill=tk.X)
         self.textbox = tk.Text(self.container,
-                                   yscrollcommand=self.scrolly.set,
-                                   xscrollcommand=self.scrollx.set,
-                                   width=18,
-                                   height=5,
-                                   wrap=tk.NONE)
+                               yscrollcommand=self.scrolly.set,
+                               xscrollcommand=self.scrollx.set,
+                               width=18,
+                               height=5,
+                               wrap=tk.NONE)
         self.textbox.pack(**LB33)
         self.scrolly.config(command=self.textbox.yview)
         self.scrollx.config(command=self.textbox.xview)
@@ -79,34 +93,50 @@ class YamlBox:
     def grid(self, *args, **kwargs):
         return self.frame.grid(*args, **kwargs)
 
+    def call_on_master(self, key, *args, **kwargs):
+        return getattr(self.frame, key)(*args, **kwargs)
 
-# Step 2c (Because 2b deployed a custom widget), Create a Dispatcher for the TkFire object
-generator = tkfire.Dispatcher({'YAML_Entry': YamlBox})
 
-# Step 3, Define the structure of the GUI
+# #### Step 2c ####
+# (Because 2b deployed a custom widget)
+# Create a Dispatcher for the TkFire object
+generator = Dispatcher({'YAML_Entry': YamlBox})
+
+
+# #### Step 3 ####
+# Define the structure of the GUI
 # 'Name of Element' (cannot contain '!')
 #      LAYOUT: [packing method ('pack' or 'grid'), args]
 #      TYPE: [name of tk, st, ttk, or custom widget, args]
 #      CHILDREN: (Optional) dict(Elements for which this element is the parent frame)
 mother = {
     'left_panel': {
-        LAYOUT: ['pack', LB33],
+        LAYOUT: ['pack', {'expand': True, **LB33}],
         TYPE: ["LabelFrame", {'text': "Left Side", 'width': 16}],
         CHILDREN: {
             'Button1': {
                 TYPE: ['Button', {'text': "Button 1"}],
-                LAYOUT: ['grid', {'row': 0, 'column': 0}],
+                LAYOUT: ['grid', {'row': 0, 'column': 0}, {'rowconfigure': {'index': 0, 'weight': 1}}],
+                # Any additional elements of a LAYOUT specification are dispatched to the Parent using
+                # the map {'command': {kwargs}, ...}
+                # So here, the parent is 'left_panel' and it's rowconfigure() method will be called with arguments:
+                # index=0 and weight=1.
             },
             'Button2': {
-                TYPE: ['Button', {'text': "Button 2", 'command': 'btn_cmd'}],
-                LAYOUT: ['grid', {'row': 1, 'column': 0}],
+                TYPE: ['Button', {'text': "Button 2!", 'command': 'btn_cmd'}],
+                LAYOUT: ['grid', {'row': 1, 'column': 0}, {'rowconfigure': {'index': 1, 'weight': 1}}],
             },
             'Button3': {
-                TYPE: ['Button', {'text': "Button 3", 'command': 'btn_cmd'}],
-                LAYOUT: ['grid', {'row': 2, 'column': 0}],
+                TYPE: ['Button', {'text': "Button 3", 'command': print_hello}],
+                LAYOUT: ['grid', {'row': 2, 'column': 0}, {'rowconfigure': {'index': 2, 'weight': 1}}],
             },
             'my_yaml_box': {
-                TYPE: ['YAML_Entry', {}],
+                TYPE: ['YAML_Entry', {},
+                       # 'YAML_Entry' can also be YamlBox or a factory which creates a YamlBox constructor.
+                       {'insert': {'where': "1.0", 'what': [1, 2, 3, 5, 8, 13]}}],
+                # Any additional elements in a TYPE specification are dispatched to the object after construction
+                # So here, after creation {my_yaml_box = YamlBox(...)}, there is an invocation of
+                # {my_yaml_box.insert(what="1.0", where=[...])}
                 LAYOUT: ['grid', {'row': 0, 'column': 1, 'rowspan': 3}]
             },
             'scrolled_text': {
@@ -125,41 +155,63 @@ mother = {
         'LAYOUT': ['pack', LB33],
         'TYPE': ["LabelFrame", {'text': "Right Side", 'width': 16}],
         'CHILDREN': {
-            'Option1': {
-                TYPE: ['OptionMenu', {'variable': ['Opt1', {'value': 0}], 'values': 'options'}],
-                LAYOUT: ['pack', TB33],
+            **{  # Variable elements can be defined with comprehensions
+                f"Option{i}": {
+                    TYPE: ['OptionMenu', {'variable': [f'Opt{i}', {'value': 0}], 'values': 'options'}],
+                    LAYOUT: ['pack', TB33],
+                } for i in range(1, n_options)
             },
-            'Option2': {
-                TYPE: ['OptionMenu', {'variable': ['Opt2', {'value': 0}], 'values': 'options'}],
-                LAYOUT: ['pack', TB33],
-            },
-            'Option3': {
-                TYPE: ['OptionMenu', {'variable': ['Opt3', {'value': 0}], 'values': 'options'}],
-                LAYOUT: ['pack', TB33],
-            },
+            f"Option{n_options}": {
+                    TYPE: ['OptionMenu', {'variable': [f'Opt{n_options}', {'value': 0}], 'values': [-1, -2, -3, -4]}],
+                    LAYOUT: ['pack', TB33],
+            }
+            # The first set of Option Menus used 'options' (a reference to memory) to specify values
+            # The final Option Menu uses
         },
     },
 }
 
-# Step 4, Create the TkFire object (if no generator is provided, it will use the default version)
-my_gui = tkfire.TkFire(core, memory, mother, generator=generator)
 
-# In order to address GUI elements after creation, use a bang-path call on the 'gui' attribute:
-my_gui.gui['left_panel!scrolled_text!my_entry'].insert(0, "hello world")
-my_gui.gui['left_panel!Button1']['text'] = "Get Yaml Input"
+# #### Step 4 ####
+# Create the TkFire object (if no generator is provided, it will use the default version)
+my_gui = TkFire(tk_core, memory, mother, generator=generator)
+
+# In order to address GUI elements after creation, use a bang-path get/set item:
+my_gui['left_panel!scrolled_text!my_entry'].insert(0, "hello world")
+my_gui['left_panel!Button1']['text'] = "Get Yaml Input"
 
 
-# For another example, a button's functionality can be bound after creation
+# #### Step 4b ####
+# Examples of editing the GUI before execution
+
+# A button's functionality can be bound after creation
 def update_test(t, v):
     t[0] = v
 
 
 test = [None, ]
-my_gui.gui['left_panel!Button1']['command'] = lambda *_: \
-    update_test(test, my_gui.gui['left_panel!my_yaml_box'].get("1.0", tk.END))
+my_gui.bind_commands(
+    ('left_panel!Button1',
+     lambda *_: update_test(test, my_gui.gui['left_panel!my_yaml_box'].get("1.0", tk.END))),
+)
 
-# Step 5, Run the gui
-core.mainloop()
+# OptionMenu widgets can be updated
+my_gui.set_optionmenu_options('right_panel!Option1', [2, 4, 6, 8])
 
+# The GUI can be modified using tkinter grammar after creation as well
+my_gui['left_panel!Button4'] = tk.Button(my_gui['left_panel'],
+                                         text="Button 4?",
+                                         command=lambda *_: print(
+                                             sum(memory[f'Opt{j}'].get() for j in range(1, n_options + 1))
+                                         )
+                                         )
+my_gui['left_panel!Button4'].grid(**grid_arg(3, 0))
+
+
+# #### Step 5 ####
+# Run the gui
+tk_core.mainloop()
+
+#
 pprint(my_gui.gui)
-pprint(test)
+pprint(test[0])
--- tkfire.py
+++ tkfire.py
@@ -7,57 +7,60 @@ reference components by Name.
 import tkinter as tk
 from tkinter import scrolledtext as st
 from tkinter import ttk
+from pprint import pformat
+
+
+__all__ = ["TkFire", "Dispatcher",
+           "grid_arg",
+           "LAYOUT", "TYPE", "CHILDREN"]
 
 
 LAYOUT = 'LAYOUT'
 TYPE = 'TYPE'
 CHILDREN = 'CHILDREN'
 PACK_SCROLL = {'side': 'right', 'fill': 'y'}
-BOTH33 = {'fill': 'both', 'ipadx': 3, 'ipady': 3}
-TB33 = {'side': 'top', 'fill': 'both', 'ipadx': 3, 'ipady': 3}
-LB33 = {'side': 'left', 'fill': 'both', 'ipadx': 3, 'ipady': 3}
 
 
 class Dispatcher:
     """ Defines the Method Resolution Order for TkFire: tk > st > ttk > custom """
-    def __init__(self, custom_elements=None):
+    def __init__(self, custom_elements=None, mro=None):
         """ Creates a dispatcher which will be provided with keywords and then determine which
         module/object to call when constructing them.
 
         **Ordering**: tkinter.tk, tkinter.scrolledtext, tkinter.ttk, custom_elements
         --> raise ModuleNotFoundError
 
-        The ordering can be changed by overwriting the __getattr__(self, item) method
-        of the Dispatcher via a  monkey-patch::
-
-        | if item in dir(tk):
-        |     return getattr(tk, item)
-        | elif item in dir(st):
-        |     return getattr(st, item)
-        | elif item in dir(ttk):
-        |     return getattr(ttk, item)
-        | elif item in self.custom_elements:
-        |     return self.custom_elements[item]
-        | else:
-        |     raise ModuleNotFoundError(item)
-
         References: :class:`TkFire`
 
         :param custom_elements: (Optional) a dict of user-made classes (keyed by class name as
           referenced in TkFire's mother)
+        :param mro: (default: tk, st, ttk) An iterable of modules, in order of method resolution
         """
         if custom_elements is None:
             custom_elements = dict()
+        if mro is None:
+            mro = [tk, st, ttk, ]
+        self._mro = mro
         self.custom_elements = custom_elements
 
+    def generate(self, item, *args, **kwargs):
+        """ Is given a constructor (or a key to dispatch to a constructor) and returns an instantiated version of
+        the object.
+
+        :param item: A constructor or a string mapped to a constructor
+        :param args: positional arguments for the constructor
+        :param kwargs: keyword arguments for the constructor
+        :return: constructor(*args, **kwargs)
+        """
+        if not isinstance(item, str):
+            return item(*args, **kwargs)
+        return getattr(self, item)(*args, **kwargs)
+
     def __getattr__(self, item):
-        if item in dir(tk):
-            return getattr(tk, item)
-        elif item in dir(st):
-            return getattr(st, item)
-        elif item in dir(ttk):
-            return getattr(ttk, item)
-        elif item in self.custom_elements:
+        for _module in self._mro:
+            if item in dir(_module):
+                return getattr(_module, item)
+        if item in self.custom_elements:
             return self.custom_elements[item]
         else:
             raise ModuleNotFoundError(item)
@@ -66,9 +69,17 @@ class Dispatcher:
 def grid_arg(row, col, kwargs=None):
     """ Used to abridge the specification of the tkinter grid packer
 
+    Reference:
+      - columnspan: How many columns a widget occupies (default 1)
+      - rowspan: How many rows a widget occupies (default 1)
+      - ipadx, ipady: How many pixels to pad inside the widget's borders, horizontally and vertically
+      - padx, pady: How many pixels to pad outside the widget's borders, horizontally and vertically
+      - sticky: What to do if the cell is larger than widget
+        (center - "", side/corner - combinations of "N", "S", "E", & "W")
+
     :param row: the row number (0-indexed)
     :param col: the column number (0-indexed)
-    :param kwargs: And additional kwargs for pack (like columnspan)
+    :param kwargs: And additional kwargs for grid (see note)
     :return: A dict of keyword arguments for grid()
     """
     if kwargs is None:
@@ -76,6 +87,14 @@ def grid_arg(row, col, kwargs=None):
     args = {'row': row, 'column': col}
     args.update(kwargs)
     return args
+
+
+class TkFireStructuralError(Exception):
+    pass
+
+
+class TkFireSpecificationError(Exception):
+    pass
 
 
 class TkFire:
@@ -141,22 +160,39 @@ class TkFire:
     | my_core.mainloop()
     """
 
-    def __init__(self, core, memory, mother, generator=Dispatcher()):
+    def __init__(self, core, memory: dict, mother: dict, generator: Dispatcher = None):
         """ Creates a TkFire object which allows a tkinter GUI to be created and modified using dictionary syntax.
 
         References: :class:`Dispatcher`
 
-        :param core: a tkinter root
+        :param core: a tkinter root (Tk, TopLevel, Frame, ...)
         :param memory: a dictionary of variables (keys may be referenced in mother)
         :param mother: a dictionary specifying the GUI
         :param generator: Determines how the objects references in mother will be constructed
         """
         self.core = core
         self.memory = memory
-        self.mother = mother
+        self._mother = mother
+
+        if generator is None:
+            generator = Dispatcher()
         self.generator = generator
-        self.gui = dict()
-        self._build("", self.mother)
+
+        self.gui = dict()  # maps to all widgets
+        self._variable_map = dict()  # Whenever variable is bound to a widget,
+        # this map stores {name_of_widget_in_gui: name_of_variable_in_memory}
+
+        self._build("", self._mother)
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            item = "!".join(item)
+        return self.gui[item]
+
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            key = "!".join(key)
+        self.gui[key] = value
 
     def _build(self, parent, structure):
         """ Constructs the dictionary which contains all the widgets (recursively)
@@ -167,47 +203,153 @@ class TkFire:
         :param structure: a (sub) dictionary of the mother
         :return: None
         """
-        if parent:
-            root = self.gui[parent]
-            path = parent + "!"
-        else:
-            root = self.core
-            path = ""
+        try:
+            if parent:
+                root = self.gui[parent]
+                path = parent + "!"
+            else:
+                root = self.core
+                path = ""
+        except Exception as e:
+            raise TkFireStructuralError(f"Failed to create path for '{parent}'") from e
+
         for k, child in structure.items():
             index = path + k
-            kwargs = child[TYPE][1]
+
+            try:
+                kwargs = child[TYPE][1]
+            except LookupError as le:
+                msg = f"Could not determine Type for '{index}' from:\n{pformat(child, depth=1)}"
+                raise TkFireSpecificationError(msg) from le
+
             args = list()
             has_scrolly = False
             has_scrollx = False
+
             if 'variable' in kwargs:
-                self.memory[kwargs['variable'][0]] = self.memory[kwargs['variable'][0]](root, **kwargs['variable'][1])
-                args.append(self.memory[kwargs['variable'][0]])
-                del child[TYPE][1]['variable']
+                try:
+                    var_name, var_spec = kwargs['variable']
+                    self.memory[var_name] = self.memory[var_name](root, **var_spec)
+                    args.append(self.memory[var_name])
+                    del child[TYPE][1]['variable']
+                    self._variable_map[index] = var_name
+                except Exception as e:
+                    msg = f"Could not parse Variable for '{index}' from:\n{pformat(kwargs['variable'])}"
+                    raise TkFireSpecificationError(msg) from e
+
             if 'command' in kwargs:
-                child[TYPE][1]['command'] = self.memory[kwargs['command']]
+                try:
+                    _command = kwargs['command']
+                    if isinstance(_command, str):
+                        child[TYPE][1]['command'] = self.memory[kwargs['command']]
+                    else:
+                        child[TYPE][1]['command'] = _command
+                except Exception as e:
+                    msg = f"Could not parse Command for '{index}' from:\n{pformat(kwargs['command'])}"
+                    raise TkFireSpecificationError(msg) from e
+
             if 'values' in kwargs:
-                args += self.memory[kwargs['values']]
-                del child[TYPE][1]['values']
-            if 'scrolly' in kwargs:
-                has_scrolly = True
-                del child[TYPE][1]['scrolly']
-            if 'scrollx' in kwargs:
-                has_scrollx = True
-                del child[TYPE][1]['scrollx']
-            self.gui[index] = getattr(self.generator, child[TYPE][0])(root, *args, **child[TYPE][1])
-            getattr(self.gui[index], child[LAYOUT][0])(**child[LAYOUT][1])
-            if has_scrolly:
-                self.gui[index + '!Scrolly'] = tk.Scrollbar(root)
-                self.gui[index]['yscrollcommand'] = self.gui[index + '!Scrolly'].set
-                self.gui[index + '!Scrolly'].config(command=self.gui[index].yview)
-                self.gui[index + '!Scrolly'].pack(**PACK_SCROLL)
-            if has_scrollx:
-                self.gui[index + '!Scrollx'] = tk.Scrollbar(root, orient=tk.HORIZONTAL)
-                self.gui[index]['xscrollcommand'] = self.gui[index + '!Scrollx'].set
-                self.gui[index + '!Scrollx'].config(command=self.gui[index].xview)
-                self.gui[index + '!Scrollx'].pack(**{'side': tk.BOTTOM, 'fill': tk.X})
+                try:
+                    _values = kwargs['values']
+                    if isinstance(_values, str):
+                        args += self.memory[kwargs['values']]
+                    else:
+                        args += _values
+                    del child[TYPE][1]['values']
+                except Exception as e:
+                    msg = f"Could not parse Values for '{index}' from:\n{pformat(kwargs['values'])}"
+                    raise TkFireSpecificationError(msg) from e
+
+            try:
+                if 'scrolly' in kwargs:
+                    has_scrolly = True
+                    del child[TYPE][1]['scrolly']
+                if 'scrollx' in kwargs:
+                    has_scrollx = True
+                    del child[TYPE][1]['scrollx']
+            except Exception as e:
+                msg = f"Could not parse scrollbar for '{index}'"
+                raise TkFireSpecificationError(msg) from e
+
+            try:
+                self.gui[index] = self.generator.generate(child[TYPE][0], root, *args, **child[TYPE][1])
+                getattr(self.gui[index], child[LAYOUT][0])(**child[LAYOUT][1])
+            except Exception as e:
+                msg = f"Could not build '{index}' from:\n{pformat(child, depth=2)}"
+                raise TkFireSpecificationError(msg) from e
+
+            if len(child[TYPE]) == 3:
+                for _meta_cmd, _meta_kwargs in child[TYPE][2].items():
+                    try:
+                        getattr(self.gui[index], _meta_cmd)(**_meta_kwargs)
+                    except Exception as e:
+                        msg = f"Could not apply meta command to '{index}' from:\n{child[TYPE]}"
+                        raise TkFireSpecificationError(msg) from e
+
+            if len(child[LAYOUT]) == 3:
+                for _meta_cmd, _meta_kwargs in child[LAYOUT][2].items():
+                    try:
+                        getattr(root, _meta_cmd)(**_meta_kwargs)
+                    except Exception as e:
+                        msg = f"Could not apply meta command to parent of '{index}' from:\n{child[LAYOUT]}"
+                        raise TkFireSpecificationError(msg) from e
+
+            try:
+                if has_scrolly:
+                    self.gui[index + '!Scrolly'] = tk.Scrollbar(root)
+                    self.gui[index]['yscrollcommand'] = self.gui[index + '!Scrolly'].set
+                    self.gui[index + '!Scrolly'].config(command=self.gui[index].yview)
+                    self.gui[index + '!Scrolly'].pack(**PACK_SCROLL)
+                if has_scrollx:
+                    self.gui[index + '!Scrollx'] = tk.Scrollbar(root, orient=tk.HORIZONTAL)
+                    self.gui[index]['xscrollcommand'] = self.gui[index + '!Scrollx'].set
+                    self.gui[index + '!Scrollx'].config(command=self.gui[index].xview)
+                    self.gui[index + '!Scrollx'].pack(**{'side': tk.BOTTOM, 'fill': tk.X})
+            except Exception as e:
+                msg = f"Could not bind scrollbars for '{index}'"
+                raise TkFireSpecificationError(msg) from e
+
             if CHILDREN in child.keys():
                 self._build(index, child[CHILDREN])
+
+    def bind_commands(self, *args):
+        """ Binds commands after
+
+        :param args: an iterable of (path, command)
+        :return: None
+        """
+        for (path, _command) in args:
+            self.gui[path]['command'] = _command
+
+    def set_optionmenu_options(self, path, options=None, *, variable=None, option_names=None):
+        """ Updates the options presented in an OptionMenu
+
+        :param path: The path to the OptionMenu
+        :param options: An iterable of options
+        :param variable: Which tkinter Variable is modified by the OptionMenu (defaults to that which was originally
+          bound to the OptionMenu)
+        :param option_names: How the options should be presented in the OptionMenu (defaults to [str(x) for x
+          in options])
+        :return: None
+        """
+        option_menu = self[path]['menu']
+        option_menu.delete(0, "end")
+
+        if options is None:
+            return
+
+        if variable is None:
+            var_name = self._variable_map[path]
+            variable = self.memory[var_name]
+
+        if option_names is None:
+            option_names = [str(opt) for opt in options]
+        elif callable(option_names):
+            option_names = [option_names(opt) for opt in options]
+
+        for opt, opt_name in zip(options, option_names):  # strict=True
+            option_menu.add_command(label=opt_name,
+                                    command=lambda value=opt: variable.set(value))
 
     def destroy(self):
         """ Calls destroy() on the tkinter core
