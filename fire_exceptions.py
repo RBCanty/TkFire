cfae18200c0395111311a6a0c9979219b89f7492
rcanty <rcanty@mit.edu>
2023-01-12 16:07:46 -0500

    playing with exception handling

--- fire_exceptions.py
+++ fire_exceptions.py
@@ -4,6 +4,13 @@ TODO: incorporate the ability to construct from a caught exception to provide mo
 
 @author: Richard "Ben" Canty
 """
+import traceback
+
+
+class TkFireException(Exception):
+    def __init__(self, base, *, message="", context=None):
+        self.__cause__ = base
+        self.args = (message, *self.args, context)
 
 
 class TkFireStructuralError(Exception):
@@ -24,3 +31,15 @@ class TkFireRenderError(Exception):
 
 class TkFirePostError(Exception):
     pass
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    try:
+        a = 4/0
+    except Exception as e:
+        try:
+            raise TkFireException(e, message="This is an exception", context="It occurred here")
+        except TkFireException as tkfe:
+            raise TkFireException(tkfe, message="Level 2")
